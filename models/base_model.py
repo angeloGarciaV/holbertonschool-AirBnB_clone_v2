1c61cd8fa542c5baadbcc09518dbc9de022c480d
Angelo Garcia <angelogarcia1029@gmail.com>
2024-03-28 16:12:22 -0400

    Added DBStorage

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
-from datetime import datetime
-from sqlalchemy import Column, Integer, String, MetaData
+from datetime import datetime, timezone
+from sqlalchemy import Column, String, DATETIME
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -11,22 +11,31 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     __tablename__ = 'basemodel'
-    id = Column(String(60), uuid.uuid4(), primary_key=True, nullable=False)
-    created_at = Column(String, datetime.now(datetime.UTC), nullable=False)
-    updated_at_at = Column(String, datetime.now(datetime.UTC))
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DATETIME, nullable=False,
+                        default=datetime.now(timezone.utc))
+    updated_at = Column(DATETIME, nullable=False,
+                        default=datetime.now(timezone.utc))
 
-    def __init__(self, id, created_at, updated_at, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            self.id = id
-            self.created_at = created_at
-            self.updated_at = updated_at
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
         else:
             for key, value in kwargs.items():
-                setattr(self, key, value)
-
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+            if not hasattr(kwargs, 'id'):
+                setattr(self, 'id', str(uuid.uuid4()))
+            if not hasattr(kwargs, 'created_at'):
+                setattr(self, 'created_at', datetime.now())
+            if not hasattr(kwargs, 'updated_at'):
+                setattr(self, 'updated_at', datetime.now())
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -46,8 +55,9 @@ class BaseModel:
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
-        if _sa_instance_state:
+        if '_sa_instance_state' in locals():
             del _sa_instance_state
+
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
