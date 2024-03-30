be3c772a4326841e7e510ff42083f04ae5759c68
Angelo Garcia <angelogarcia1029@gmail.com>
2024-03-29 22:10:28 -0400

    Fix for Review class

--- models/review.py
+++ models/review.py
@@ -8,11 +8,6 @@ from sqlalchemy import Column, String, ForeignKey
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        place_id = Column(String(60), ForeignKey('place.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
-        text = Column(String(1024), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    place_id = Column(String(60), ForeignKey('place.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
+    text = Column(String(1024), nullable=False)
