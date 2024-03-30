5ef8938761a9c1bf091e40c45cbdd3dccf46f938
Angelo Garcia <angelogarcia1029@gmail.com>
2024-03-29 20:16:57 -0400

    Fix for amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -2,65 +2,17 @@
 """ State Module for HBNB project """
 import os
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Table, String
-from sqlalchemy import Integer, Float, ForeignKey, MetaData
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
-metadata_obj = MetaData()
-place_amenity = Table('place_amenity', metadata_obj,
-                      Column('place_id', String(60), ForeignKey(
-                          'places.id'), primary_key=True, nullable=False),
-                      Column('amenity_id', String(60), ForeignKey(
-                          'amenities_id'), primary_key=True, nullable=False))
 
 
 class Amenity(BaseModel, Base):
     """This class defines an amenity by various attributes"""
     __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=False)
-        number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float)
-        longitude = Column(Float)
-    else:
-        city_id = ''
-        user_id = ''
-        name = ''
-        description = ''
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        amenities = relationship(
-            'Amenity',
-            secondary=place_amenity,
-            viewonly=False,
-            backref='place_amenities'
+        place_amenities = relationship(
+            "Place",
+            secondary='place_amenity',
+            back_populates='amenities'
         )
-    else:
-        @property
-        def amenities(self):
-            """Returns the amenities of this Place"""
-            from models import storage
-            amenities_of_place = []
-            for value in storage.all(Amenity).values():
-                if value.id in self.amenity_ids:
-                    amenities_of_place.append(value)
-            return amenities_of_place
-
-        @amenities.setter
-        def amenities(self, value):
-            """Adds an amenity to this Place"""
-            if type(value) is Amenity:
-                if value.id not in self.amenity_ids:
-                    self.amenity_ids.append(value.id)
