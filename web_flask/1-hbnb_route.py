86995d7944c9c5d3957ec51c5ec951ffde85948c
Angelo Garcia <angelogarcia1029@gmail.com>
2024-04-08 09:47:25 -0400

    Created Web_Flask directory. README.md and 1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module to start a Flask web application
+
+    Returns:
+        string: a web page
+    """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def HelloHBNB():
+    """Function to start a Flask web app with a return of 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """Function to start a Flask web app with a return of '/hbnb'"""
+    return "/hbnb"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
