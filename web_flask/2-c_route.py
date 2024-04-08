9dd4ead2d2ae54205835d3f8598264cb2c13d13c
Angelo Garcia <angelogarcia1029@gmail.com>
2024-04-08 10:33:29 -0400

    Added C-route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module to start a Flask web application
+
+    Returns:
+        string: a web page
+    """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """Function to start a Flask web app with a return of 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Function to start a Flask web app with a return of '/hbnb'"""
+    return "/hbnb"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def dispC(text):
+    return f'C {text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
