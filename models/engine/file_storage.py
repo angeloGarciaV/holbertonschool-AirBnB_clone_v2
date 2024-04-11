f8894ced45f6ae1e2274e686a138bc6970fa3a32
Angelo Garcia <angelogarcia1029@gmail.com>
2024-04-11 17:33:18 -0400

    file_storage update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,11 +28,12 @@ class FileStorage:
         """returns the dictionary __objects"""
         if cls is not None:
             new_dict = {}
-            for key, value in self.__objects.items():
-                if cls == value.__class__ or cls == value.__class__.__name__:
+            for key, value in FileStorage.__objects.items():
+                if cls == value.__class__:
                     new_dict[key] = value
             return new_dict
-        return self.__objects
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
@@ -61,9 +62,11 @@ class FileStorage:
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
         if obj is not None:
-            key = obj.__class__.__name__ + '.' + obj.id
+            key = str(obj.__class__.__name__) + '.' + (obj.id)
             if key in self.__objects:
-                del self.__objects[key]
+                del FileStorage.__objects[key]
+        else:
+            pass
 
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
