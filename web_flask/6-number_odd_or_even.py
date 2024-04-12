29cdc80548c1bf1d8936ef03be8ac3d762da3ab0
Angelo Garcia <angelogarcia1029@gmail.com>
2024-04-12 19:12:47 -0400

    Refactor Flask routes to remove strict slashes and fix number routes

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -4,67 +4,55 @@
     Returns:
         string: a web page
     """
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def index():
     """Function to start a Flask web app with a return of 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb")
 def hbnb():
     """Function to start a Flask web app with a return of '/hbnb'"""
-    return "/hbnb"
+    return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route("/c/<text>")
 def dispC(text):
     """Function with a return of '/c/<text>'"""
     newText = text.replace('_', " ")
     return f'C {newText}'
 
 
-@app.route("/python", defaults={'text': 'is_cool'}, strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python", defaults={'text': 'is_cool'})
+@app.route("/python/<text>")
 def dispPython(text):
     """Function with a return of '/python/<text>'"""
     newText = text.replace('_', " ")
     return f'Python {newText}'
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>")
 def ifInt(n):
     """display “n is a number” only if n is an integer"""
-    try:
-        number = int(n)
-    except Exception:
-        return f'{n} is not a number'
-    return f'{number} is a number'
+    return f'{n} is a number'
 
 
-@app.route("/number_template/<n>", strict_slashes=False)
-def numberTemplate(n):
-    """render 5-number.html if n is a number"""
-    try:
-        number = int(n)
-    except Exception:
-        return f"Can't render template. {n} is not a number"
-    return render_template("5-number.html", tempNumber=number)
+@app.route("/number_template/<int:n>")
+def number_template(n):
+    """display “n is a number” only if n is an integer"""
+    return render_template("5-number.html", n=n)
 
 
-@app.route("/number_odd_or_even/<n>", strict_slashes=False)
-def oddOrEven(n):
-    """Display wether n is odd or even by rendering in html"""
-    try:
-        number = int(n)
-    except Exception as e:
-        print(f"Error: {e}")
-        return f"Can't render template. {n} is not a number"
-    return render_template("6-number_odd_or_even.html", oddEvenNum=number)
+@app.route("/number_odd_or_even/<int:n>")
+def odd_or_even(n):
+    """display whether n is odd or even in a template"""
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == '__main__':
