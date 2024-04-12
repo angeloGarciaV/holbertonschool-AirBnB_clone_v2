0ba87ed5174dab9dae26087f125eae181261e336
Angelo Garcia <angelogarcia1029@gmail.com>
2024-04-12 19:15:57 -0400

    Refactor Flask routes to remove strict slashes and fix number routes

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,51 +1,45 @@
 #!/usr/bin/python3
-"""Module to start a Flask web application
-
-    Returns:
-        string: a web page
-    """
-from flask import Flask, render_template, abort
+""" starts the flask app """
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/")
+@app.route('/', strict_slashes=False)
 def index():
-    """Function to start a Flask web app with a return of 'Hello HBNB!'"""
-    return "Hello HBNB!"
+    """ returns hello HBNB """
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb")
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Function to start a Flask web app with a return of '/hbnb'"""
-    return "HBNB"
+    """ returns HBNB """
+    return 'HBNB'
 
 
-@app.route("/c/<text>")
+@app.route('/c/<text>', strict_slashes=False)
 def dispC(text):
-    """Function with a return of '/c/<text>'"""
-    newText = text.replace('_', " ")
-    return f'C {newText}'
+    """ returns C """
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route("/python", defaults={'text': 'is_cool'})
-@app.route("/python/<text>")
+@app.route('/python/', defaults={'text': "is cool"}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def dispPython(text):
-    """Function with a return of '/python/<text>'"""
-    newText = text.replace('_', " ")
-    return f'Python {newText}'
+    """ returns python """
+    return 'Python ' + text.replace('_', ' ')
 
 
-@app.route("/number/<int:n>")
+@app.route('/number/<int:n>', strict_slashes=False)
 def ifInt(n):
-    """display “n is a number” only if n is an integer"""
-    return f'{n} is a number'
+    """ returns the number """
+    return f"{n} is a number"
 
 
-@app.route("/number_template/<int:n>")
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """display “n is a number” only if n is an integer"""
+    """ display html only if n is an integer """
     return render_template("5-number.html", n=n)
 
 
