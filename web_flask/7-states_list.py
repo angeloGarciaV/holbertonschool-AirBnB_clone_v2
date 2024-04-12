9e1ed682e77d6581a1b4cf8621cd300c9a3151d6
Angelo Garcia <angelogarcia1029@gmail.com>
2024-04-12 19:52:35 -0400

    Refactor Flask routes to remove strict slashes and fix number routes

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,19 +8,65 @@ from models import storage
 from models.state import State
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    """Function to start a Flask web app with a return of 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """Function to start a Flask web app with a return of '/hbnb'"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def dispC(text):
+    """Function with a return of '/c/<text>'"""
+    newText = text.replace('_', " ")
+    return f'C {newText}'
+
+
+@app.route("/python", defaults={'text': 'is_cool'})
+@app.route("/python/<text>")
+def dispPython(text):
+    """Function with a return of '/python/<text>'"""
+    newText = text.replace('_', " ")
+    return f'Python {newText}'
+
+
+@app.route("/number/<int:n>")
+def ifInt(n):
+    """display “n is a number” only if n is an integer"""
+    return f'{n} is a number'
+
+
+@app.route("/number_template/<int:n>")
+def number_template(n):
+    """display “n is a number” only if n is an integer"""
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>")
+def odd_or_even(n):
+    """display whether n is odd or even in a template"""
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+@app.route('/states_list')
+def states_list():
+    """Display a HTML page"""
+    state_list = storage.all(State).values()
+    return render_template('7-states_list.html', states=state_list)
 
 
 @app.teardown_appcontext
-def handle_appcontext(self):
-    """Method for handling app context"""
+def teardown_appcontext(exception):
+    """After each request, remove the SQLAlchemy Session"""
     storage.close()
-
-
-@app.route('/states_list', strict_slashes=False)
-def list_states():
-    """Displays 7-states_list html page"""
-    states = storage.all(State).values()
-    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
