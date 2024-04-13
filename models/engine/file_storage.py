a7c4bb5192e806949d8752611524ff8f12ff7b71
Angelo Garcia <angelogarcia1029@gmail.com>
2024-04-12 21:55:21 -0400

    Add import statements for missing model classes in FileStorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
+from models.review import Review
+from models.amenity import Amenity
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.user import User
+from models.base_model import BaseModel
 import json
 
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
+
     __file_path = 'file.json'
     __objects = {}
 
@@ -33,15 +41,7 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
+        """Reloads objects from file"""
         classes = {
             'BaseModel': BaseModel, 'User': User, 'Place': Place,
             'State': State, 'City': City, 'Amenity': Amenity,
