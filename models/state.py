74f65521a4567b08a024927f220e671a765d2204
Angelo Garcia <angelogarcia1029@gmail.com>
2024-04-12 22:46:59 -0400

    Refactor State class in state.py for improved code organization and documentation

--- models/state.py
+++ models/state.py
@@ -9,12 +9,10 @@ from models.city import City
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
+    """State class"""
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship("City", cascade='all, delete, delete-orphan',
                               backref="state")
